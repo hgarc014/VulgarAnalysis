72e21a28e72160157ff69e59cfc7ff0d7ec96e41
Henry Garcia <hgarc014@ucr.edu>
2017-04-16 13:44:42 -0700

    Added mave

--- part1/TweetCrawler/src/crawler/Main.java
+++ part1/TweetCrawler/src/crawler/Main.java
@@ -13,14 +13,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
-import edu.stanford.nlp.ling.CoreAnnotations;
-import edu.stanford.nlp.neural.rnn.RNNCoreAnnotations;
-import edu.stanford.nlp.pipeline.Annotation;
-import edu.stanford.nlp.pipeline.StanfordCoreNLP;
-import edu.stanford.nlp.sentiment.SentimentCoreAnnotations;
-import edu.stanford.nlp.sentiment.SentimentCoreAnnotations.SentimentAnnotatedTree;
-import edu.stanford.nlp.trees.Tree;
-import edu.stanford.nlp.util.CoreMap;
 
 
 public class Main extends Thread {
@@ -31,88 +23,23 @@ public class Main extends Thread {
 		int threads = 1;
 		int runForSeconds = 10;
 
-//		if(args.length == 1){
-//			runForSeconds = checkNumber(args[0], "Number of Seconds");
-//
-//			CrawlerInformation info = new CrawlerInformation(threads, saveEvery,runForSeconds);
-//			info.setdbConnection("localhost", "3306", "henry", "PH.h3143,twitter");
-//
-//			Crawler c = new Crawler(info);
-//			c.run();
-//
-//		}else{
-//			 System.out.println("Invalid number of arguments");
-//			 System.out.println("Arguments: " + args.length);
-//			 System.out.println("<Number of Seconds>");			
-//		}
-		
-		CrawlerInformation info = new CrawlerInformation(threads, 100, 10);
-		info.setdbConnection("localhost", "3306", "henry", "PH.h3143,twitter");
-		
-		Statement st;
-		ResultSet rs ;
-		try {
-			st = info.getDatabase().createStatement();
-			rs = st.executeQuery("SELECT * FROM TwitterCrawled.tweets");
-			
-			Set<Integer> s = new HashSet<>();
-			String tweet  = null;
-//			String regex = "^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
-	        String regex = "(http?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]"; // does not match <http://google.com>
-	        String regex_https = "(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]"; // does not match <http://google.com>
-	        
-	        Integer res;
-			while(rs.next()){
-				tweet = rs.getString("tweet_text");
-				tweet = tweet.replaceAll(regex, "");
-				tweet = tweet.replaceAll(regex_https, "");
-				res = findSentiment(tweet);
-				if(!s.contains(res)){
-					s.add(res);
-				}
-//				System.out.println(findSentiment(tweet) + ":" + tweet );
-			}
-			
-			for(Integer i :s){
-				System.out.println(i);
-			}
-//			System.out.println("BAD:"+findSentiment("Fuck you, asshole."));
-//			System.out.println("Good:"+findSentiment("Whoever you are, you are really chill and awesome."));
-//			System.out.println("Good:"+findSentiment("Love is the best thinig in the world."));
-//			System.out.println("Good:"+findSentiment("I love you."));
-			
-			
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		if(args.length == 1){
+			runForSeconds = checkNumber(args[0], "Number of Seconds");
+
+			CrawlerInformation info = new CrawlerInformation(threads, saveEvery,runForSeconds);
+			info.setdbConnection("localhost", "3306", "henry", "PH.h3143,twitter");
+
+			Crawler c = new Crawler(info);
+			c.run();
+
+		}else{
+			 System.out.println("Invalid number of arguments");
+			 System.out.println("Arguments: " + args.length);
+			 System.out.println("<Number of Seconds>");			
 		}
+		
 	}
 	
-	static StanfordCoreNLP pipeline = null;
-	public static int findSentiment(String tweet){
-		if(pipeline == null){
-			pipeline = new StanfordCoreNLP("MyPropFile.properties");
-		}
-		int mainSentiment = 0;
-		if (tweet != null && tweet.length() > 0) {
-			int longest = 0;
-			Annotation annotation = pipeline.process(tweet);
-			for (CoreMap sentence : annotation
-					.get(CoreAnnotations.SentencesAnnotation.class)) {
-				Tree tree = sentence
-						.get(SentimentAnnotatedTree.class);
-				int sentiment = RNNCoreAnnotations.getPredictedClass(tree);
-				String partText = sentence.toString();
-				if (partText.length() > longest) {
-					mainSentiment = sentiment;
-					longest = partText.length();
-				}
-
-			}
-		}
-		return mainSentiment;	
-	}
-
 	public static Integer checkNumber(String convert, String name) {
 		try {
 			int x = Integer.valueOf(convert);
